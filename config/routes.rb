c9a3b420d02534bd2c7f44b298e60a55ba4c9b00
uberkami <uberkami@gmail.com>
2019-12-21 00:13:43 +0100

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+#Rails.application.routes.draw do
+#  resources :cars
+#  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
 Rails.application.routes.draw do
+  root 'cars#index'
   resources :cars
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
